87fc6f81f82d065a6fc7a2114b9eaa02da40d7f2
rjrodger <richard@ricebridge.com>
2019-10-11 23:28:10 +0100

    initial

---
+++ jostraca.js
@@ -0,0 +1,12 @@
+/* Copyright © 2019 Richard Rodger and other contributors, MIT License. */
+
+module.exports = make_jostraca
+
+
+function make_jostraca() {
+  return new Jostraca()
+}
+
+function Jostraca() {
+
+}
