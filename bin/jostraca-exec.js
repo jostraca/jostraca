99c5f31291cf19c53bf569c38c27f9dbec96483f
rjrodger <richard@ricebridge.com>
2019-10-12 00:03:20 +0100

    v0.0.4

---
+++ bin/jostraca-exec.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+/* Copyright (c) 2019 Richard Rodger and other contributors, MIT License */
+'use strict'
+
+const Jostraca = require('..')
+
+const js = Jostraca()
+
+console.log(''+js)
