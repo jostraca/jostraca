d30ebcfb10cb4d7f82fcd07254c175544e8396b0
Richard Rodger <richard@ricebridge.com>
2024-12-10 21:00:29 +0000

    copy-props-type-check

--- dist/cmp/Copy.js
+++ dist/cmp/Copy.js
@@ -2,7 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Copy = void 0;
 const jostraca_1 = require("../jostraca");
+const gubu_1 = require("gubu");
+const From = (from, _, s) => s.ctx.fs.statSync(from);
+const CopyShape = (0, gubu_1.Gubu)({
+    ctx$: Object,
+    name: (0, gubu_1.Optional)(String),
+    from: (0, gubu_1.Check)(From).String(),
+    exclude: (0, gubu_1.Optional)((0, gubu_1.One)(Boolean, [String]))
+}, { name: 'Copy' });
 const Copy = (0, jostraca_1.cmp)(function Copy(props, _children) {
+    props = CopyShape(props, { fs: props.ctx$.fs });
     const node = props.ctx$.node;
     node.kind = 'copy';
     node.name = props.name;
--- src/cmp/Copy.ts
+++ src/cmp/Copy.ts
@@ -3,9 +3,26 @@ import type { Node } from '../jostraca'
 
 import { cmp } from '../jostraca'
 
+import { Gubu, One, Optional, Check } from 'gubu'
 
-const Copy = cmp(function Copy(props: any, _children: any) {
+
+const From = (from: any, _: any, s: any) => s.ctx.fs.statSync(from)
+
+const CopyShape = Gubu({
+  ctx$: Object,
+  name: Optional(String),
+  from: Check(From).String(),
+  exclude: Optional(One(Boolean, [String]))
+}, { name: 'Copy' })
+
+
+type CopyProps = ReturnType<typeof CopyShape>
+
+const Copy = cmp(function Copy(props: CopyProps, _children: any) {
+  props = CopyShape(props, { fs: props.ctx$.fs })
+
   const node: Node = props.ctx$.node
+
   node.kind = 'copy'
   node.name = props.name
   node.from = props.from
