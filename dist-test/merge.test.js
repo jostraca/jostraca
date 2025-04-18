e2565535c4d113e547891e5b0f540f08e399ec61
Richard Rodger <richard@ricebridge.com>
2025-04-18 17:58:16 +0100

    test

---
+++ dist-test/merge.test.js
@@ -0,0 +1,39 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_test_1 = require("node:test");
+const code_1 = require("@hapi/code");
+const memfs_1 = require("memfs");
+const __1 = require("../");
+(0, node_test_1.describe)('merge', () => {
+    (0, node_test_1.test)('happy', async () => {
+        (0, code_1.expect)(__1.Jostraca).exist();
+        const jostraca = (0, __1.Jostraca)();
+        (0, code_1.expect)(jostraca).exist();
+        const { fs, vol } = (0, memfs_1.memfs)({});
+        const info = await jostraca.generate({ fs: () => fs, folder: '/top' }, () => (0, __1.Project)({ folder: 'sdk' }, () => {
+            (0, __1.Folder)({ name: 'js' }, () => {
+                (0, __1.File)({ name: 'foo.js' }, () => {
+                    (0, __1.Content)('// custom-foo\n');
+                });
+                (0, __1.File)({ name: 'bar.js' }, () => {
+                    (0, __1.Content)('// custom-bar\n');
+                });
+            });
+            (0, __1.Folder)({ name: 'go' }, () => {
+                (0, __1.File)({ name: 'zed.go' }, () => {
+                    (0, __1.Content)('// custom-zed\n');
+                });
+            });
+        }));
+        // console.log('INFO', info)
+        const voljson = vol.toJSON();
+        (0, code_1.expect)(JSON.parse(voljson['/top/.jostraca/jostraca.json.log']).exclude).equal([]);
+        (0, code_1.expect)(voljson).equal({
+            '/top/.jostraca/jostraca.json.log': voljson['/top/.jostraca/jostraca.json.log'],
+            '/top/sdk/js/foo.js': '// custom-foo\n',
+            '/top/sdk/js/bar.js': '// custom-bar\n',
+            '/top/sdk/go/zed.go': '// custom-zed\n'
+        });
+    });
+});
+//# sourceMappingURL=merge.test.js.map
---
+++ test/merge.test.ts
@@ -0,0 +1,74 @@
+
+import { test, describe } from 'node:test'
+import { expect } from '@hapi/code'
+
+import { memfs } from 'memfs'
+
+
+import {
+  Jostraca,
+  Project,
+  Folder,
+  File,
+  Fragment,
+  Content,
+  Copy,
+  Inject,
+  Line,
+  Slot,
+
+  cmp,
+  each,
+} from '../'
+
+
+describe('merge', () => {
+
+  test('happy', async () => {
+    expect(Jostraca).exist()
+
+    const jostraca = Jostraca()
+    expect(jostraca).exist()
+
+    const { fs, vol } = memfs({})
+
+    const info = await jostraca.generate(
+      { fs: () => fs, folder: '/top' },
+      () => Project({ folder: 'sdk' }, () => {
+
+        Folder({ name: 'js' }, () => {
+
+          File({ name: 'foo.js' }, () => {
+            Content('// custom-foo\n')
+          })
+
+          File({ name: 'bar.js' }, () => {
+            Content('// custom-bar\n')
+          })
+        })
+
+        Folder({ name: 'go' }, () => {
+
+          File({ name: 'zed.go' }, () => {
+            Content('// custom-zed\n')
+          })
+        })
+
+      })
+    )
+
+    // console.log('INFO', info)
+    const voljson: any = vol.toJSON()
+
+    expect(JSON.parse(voljson['/top/.jostraca/jostraca.json.log']).exclude).equal([])
+    expect(voljson).equal({
+      '/top/.jostraca/jostraca.json.log': voljson['/top/.jostraca/jostraca.json.log'],
+      '/top/sdk/js/foo.js': '// custom-foo\n',
+      '/top/sdk/js/bar.js': '// custom-bar\n',
+      '/top/sdk/go/zed.go': '// custom-zed\n'
+    })
+  })
+
+
+})
+
